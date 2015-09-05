e415c60c96df497e5e9819e5cdeb1744aa4ad55e
Sergio Ormeño <sergioignacio.ov@hotmail.com>
2015-09-05 11:27:59 -0400

    Bootstrap

--- AlTuristicos/settings.py
+++ AlTuristicos/settings.py
@@ -101,3 +101,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "staticf"),
+    #'/var/www/static/',
+)
+
+STATIC_ROOT = "/var/www/example.com/static/"
